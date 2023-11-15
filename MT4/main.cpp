a1fba2e73c599ed6f9c6631f5ce02dd29f4eb73b
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-15 14:06:46 +0900

    途中a

--- MT4/main.cpp
+++ MT4/main.cpp
@@ -10,7 +10,8 @@
 #include <algorithm>
 
 const char kWindowTitle[] = "MT4";
-
+static const int kRowHeight = 20;
+static const int kColumnWidth = 80;
 
 
 struct Quaternion final {
@@ -19,6 +20,310 @@ struct Quaternion final {
 	//float z;
 	Vector3 v;
 	float w;
+};
+
+
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2) {
+
+	Matrix4x4 result;
+
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			result.m[y][x] = m1.m[y][x] + m2.m[y][x];
+		}
+	}
+	return result;
+}
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result;
+
+	result.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
+	result.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
+	result.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
+	result.m[0][3] = m1.m[0][0] * m2.m[0][3] + m1.m[0][1] * m2.m[1][3] + m1.m[0][2] * m2.m[2][3] + m1.m[0][3] * m2.m[3][3];
+
+	result.m[1][0] = m1.m[1][0] * m2.m[0][0] + m1.m[1][1] * m2.m[1][0] + m1.m[1][2] * m2.m[2][0] + m1.m[1][3] * m2.m[3][0];
+	result.m[1][1] = m1.m[1][0] * m2.m[0][1] + m1.m[1][1] * m2.m[1][1] + m1.m[1][2] * m2.m[2][1] + m1.m[1][3] * m2.m[3][1];
+	result.m[1][2] = m1.m[1][0] * m2.m[0][2] + m1.m[1][1] * m2.m[1][2] + m1.m[1][2] * m2.m[2][2] + m1.m[1][3] * m2.m[3][2];
+	result.m[1][3] = m1.m[1][0] * m2.m[0][3] + m1.m[1][1] * m2.m[1][3] + m1.m[1][2] * m2.m[2][3] + m1.m[1][3] * m2.m[3][3];
+
+	result.m[2][0] = m1.m[2][0] * m2.m[0][0] + m1.m[2][1] * m2.m[1][0] + m1.m[2][2] * m2.m[2][0] + m1.m[2][3] * m2.m[3][0];
+	result.m[2][1] = m1.m[2][0] * m2.m[0][1] + m1.m[2][1] * m2.m[1][1] + m1.m[2][2] * m2.m[2][1] + m1.m[2][3] * m2.m[3][1];
+	result.m[2][2] = m1.m[2][0] * m2.m[0][2] + m1.m[2][1] * m2.m[1][2] + m1.m[2][2] * m2.m[2][2] + m1.m[2][3] * m2.m[3][2];
+	result.m[2][3] = m1.m[2][0] * m2.m[0][3] + m1.m[2][1] * m2.m[1][3] + m1.m[2][2] * m2.m[2][3] + m1.m[2][3] * m2.m[3][3];
+
+	result.m[3][0] = m1.m[3][0] * m2.m[0][0] + m1.m[3][1] * m2.m[1][0] + m1.m[3][2] * m2.m[2][0] + m1.m[3][3] * m2.m[3][0];
+	result.m[3][1] = m1.m[3][0] * m2.m[0][1] + m1.m[3][1] * m2.m[1][1] + m1.m[3][2] * m2.m[2][1] + m1.m[3][3] * m2.m[3][1];
+	result.m[3][2] = m1.m[3][0] * m2.m[0][2] + m1.m[3][1] * m2.m[1][2] + m1.m[3][2] * m2.m[2][2] + m1.m[3][3] * m2.m[3][2];
+	result.m[3][3] = m1.m[3][0] * m2.m[0][3] + m1.m[3][1] * m2.m[1][3] + m1.m[3][2] * m2.m[2][3] + m1.m[3][3] * m2.m[3][3];
+
+	return result;
+}
+
+Matrix4x4 Multiply(float scalar, const Matrix4x4& m)
+{
+
+	Matrix4x4 result;
+
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			result.m[y][x] = scalar * m.m[y][x];
+		}
+	}
+	return result;
+}
+//拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale)
+{
+	Matrix4x4 result;
+
+	result.m[0][0] = scale.x;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = scale.y;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = scale.z;
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = 0.0f;
+	result.m[3][1] = 0.0f;
+	result.m[3][2] = 0.0f;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+//  X軸回転
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	Matrix4x4 result;
+
+	result.m[0][0] = 1;
+	result.m[0][1] = 0;
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = std::sin(radian);
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = -std::sin(radian);
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+// Y軸回転
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	Matrix4x4 result;
+
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = 0;
+	result.m[0][2] = -std::sin(radian);
+	result.m[0][3] = 0;
+
+	result.m[1][0] = 0;
+	result.m[1][1] = 1;
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = std::sin(radian);
+	result.m[2][1] = 0;
+	result.m[2][2] = std::cos(radian);
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+// Z軸回転
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	Matrix4x4 result;
+
+	result.m[0][0] = std::cos(radian);
+	result.m[0][1] = std::sin(radian);
+	result.m[0][2] = 0;
+	result.m[0][3] = 0;
+
+	result.m[1][0] = -std::sin(radian);
+	result.m[1][1] = std::cos(radian);
+	result.m[1][2] = 0;
+	result.m[1][3] = 0;
+
+	result.m[2][0] = 0;
+	result.m[2][1] = 0;
+	result.m[2][2] = 1;
+	result.m[2][3] = 0;
+
+	result.m[3][0] = 0;
+	result.m[3][1] = 0;
+	result.m[3][2] = 0;
+	result.m[3][3] = 1;
+
+	return result;
+}
+// 回転行列
+Matrix4x4 MakeRotateMatrix(const Vector3& radian) {
+	Matrix4x4 result;
+	Matrix4x4 RotateX = MakeRotateXMatrix(radian.x);
+	Matrix4x4 RotateY = MakeRotateYMatrix(radian.y);
+	Matrix4x4 RotateZ = MakeRotateZMatrix(radian.z);
+
+	result = Multiply(RotateX, Multiply(RotateY, RotateZ));
+
+	return result;
+}
+
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 result;
+
+	result.m[0][0] = 1.0f;
+	result.m[0][1] = 0.0f;
+	result.m[0][2] = 0.0f;
+	result.m[0][3] = 0.0f;
+
+	result.m[1][0] = 0.0f;
+	result.m[1][1] = 1.0f;
+	result.m[1][2] = 0.0f;
+	result.m[1][3] = 0.0f;
+
+	result.m[2][0] = 0.0f;
+	result.m[2][1] = 0.0f;
+	result.m[2][2] = 1.0f;
+	result.m[2][3] = 0.0f;
+
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+//アフィン行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rot, const Vector3& translate)
+{
+	Matrix4x4 result;
+
+	// スケーリング行列＊回転行列＊平行移動行列
+	result = Multiply(MakeScaleMatrix(scale), Multiply(MakeRotateMatrix(rot), MakeTranslateMatrix(translate)));
+
+	return result;
+}
+
+//単位行列の作成
+Matrix4x4 MakeIdentity4x4() {
+
+	Matrix4x4 result;
+
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (y == x) {
+				result.m[y][x] = 1.0f;
+			}
+			else {
+				result.m[y][x] = 0.0f;
+			}
+		}
+	}
+
+	return result;
+}
+
+Matrix4x4 MakeRotateAxisAngle(const Vector3& axis, float angle)
+{
+	//S
+	Matrix4x4 matrixS = MakeIdentity4x4();
+	matrixS.m[0][0] = std::cosf(angle);
+	matrixS.m[1][1] = std::cosf(angle);
+	matrixS.m[2][2] = std::cosf(angle);
+
+	//P
+	Matrix4x4 matrixP = MakeIdentity4x4();
+	matrixP.m[0][0] = axis.x * axis.x;
+	matrixP.m[0][1] = axis.x * axis.y;
+	matrixP.m[0][2] = axis.x * axis.z;
+	matrixP.m[1][0] = axis.y * axis.x;
+	matrixP.m[1][1] = axis.y * axis.y;
+	matrixP.m[1][2] = axis.y * axis.z;
+	matrixP.m[2][0] = axis.z * axis.x;
+	matrixP.m[2][1] = axis.z * axis.y;
+	matrixP.m[2][2] = axis.z * axis.z;
+	matrixP.m[3][3] = 0.0f;
+	matrixP = Multiply((1.0f - std::cosf(angle)), matrixP);
+
+	//C
+	Matrix4x4 matrixC = MakeIdentity4x4();
+	matrixC.m[0][0] = 0.0f;
+	matrixC.m[0][1] = -axis.z;
+	matrixC.m[0][2] = axis.y;
+	matrixC.m[1][0] = axis.z;
+	matrixC.m[1][1] = 0.0f;
+	matrixC.m[1][2] = -axis.x;
+	matrixC.m[2][0] = -axis.y;
+	matrixC.m[2][1] = axis.x;
+	matrixC.m[2][2] = 0.0f;
+	matrixC.m[3][3] = 0.0f;
+	matrixC = Multiply(-std::sinf(angle), matrixC);
+
+	// result
+	Matrix4x4 resultMatrix = Add(Add(matrixS, matrixP), matrixC);
+
+	return resultMatrix;
+}
+
+Vector3 Multiply(float scalar, const Vector3& v) {
+
+	Vector3 result;
+
+	result = { scalar * v.x, scalar * v.y, scalar * v.z };
+
+	return result;
+
+}
+
+//内積
+float Dot(const Vector3& v1, const Vector3& v2) {
+
+	float result;
+
+	result = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+
+	return result;
+
+}
+Vector3 Cross(const Vector3& v1, const Vector3& v2) {
+
+	Vector3 result = { v1.y * v2.z - v1.z * v2.y,v1.z * v2.x - v1.x * v2.z,v1.x * v2.y - v1.y * v2.x, };
+
+	return result;
+
+}
+
+// 長さ(ノルム)
+float Length(const Vector3& v) {
+	float result;
+	result = powf(v.x, 2.0) + powf(v.y, 2.0) + powf(v.z, 2.0);
+
+	return sqrtf(result);
 };
 
 
@@ -386,7 +691,6 @@ Vector3 Slerp(const Vector3& v1, const Vector3& v2, float t)
 	return s * ((std::sinf((1.0f - t) * theta) / std::sinf(theta)) * a + (std::sinf(t * theta) / std::sinf(theta)) * b);
 }
 
-
 Quaternion Multiply(const Quaternion& q, const Quaternion& r) {
 	Quaternion result;
 	result.w = q.w * r.w - Dot(q.v, r.v);
@@ -402,8 +706,29 @@ Quaternion Conjugate(const Quaternion& q) {
 	return Quaternion{ (-1.0f * q.v),q.w };
 }
 
-const char kWindowTitle[] = "学籍番号";
+float Norm(const Quaternion& q) {
+	return std::sqrtf(q.w * q.w + q.v.x * q.v.x + q.v.y * q.v.y + q.v.z * q.v.z);
+}
 
+Quaternion Normalize(const Quaternion& q) {
+	Quaternion result = q;
+	float norm = Norm(q);
+	result.w /= norm;
+	result.v.x /= norm;
+	result.v.y /= norm;
+	result.v.z /= norm;
+	return result;
+}
+
+Quaternion Inverse(const Quaternion& q) {
+	Quaternion result = Conjugate(q);
+	float length = Norm(q) * Norm(q);
+	result.w /= length;
+	result.v.x /= length;
+	result.v.y /= length;
+	result.v.z /= length;
+	return result;
+}
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
