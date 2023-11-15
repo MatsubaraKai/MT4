4d45443d632e85fac965cea2634360999aed43ec
MatsubaraKai <k022g0104@g.neec.ac.jp>
2023-11-15 14:06:46 +0900

    プロジェクト ファイルを追加します。

--- MT4/main.cpp
+++ MT4/main.cpp
@@ -1,6 +1,6 @@
 #include <Novice.h>
 
-const char kWindowTitle[] = "学籍番号";
+const char kWindowTitle[] = "MT4";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
